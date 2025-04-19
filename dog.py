79ee231fa095862555358c64ef86073877eef1cd
vivekkapar5 <vivekkapar@mail.weber.edu>
2025-04-19 17:27:21 -0600

    Update dog.py

--- dog.py
+++ dog.py
@@ -1,14 +1,16 @@
-class Dog:
+from animal import Animal
+
+class Dog(Animal):
     """
     Derived class representing a dog, which is a type of Animal.
     """
-    # TODO: Initialize the Dog class and add the breed attribute.
-    # The constructor should accept name, species, and breed as parameters.
-    
-    # TODO: Override the __str__ method to include the breed.
-    # Example output:
-    # Kingdom: 'kingdom attribute', Name: 'name attribute', Species: 'species attribute', Breed: 'breed attribute'
-    
-    # TODO: Add a method for the dog to make a specific sound. 
-    # Call the method `speak` and make it output a specific message like 
-    # "The dog barks.
+    def __init__(self, name, species, breed):
+        # Call parent class initializer
+        super().__init__(name, species)
+        self.breed = breed
+
+    def __str__(self):
+        return f"Kingdom: '{self.kingdom}', Name: '{self.name}', Species: '{self.species}', Breed: '{self.breed}'"
+
+    def speak(self):
+        print("The dog barks.")
